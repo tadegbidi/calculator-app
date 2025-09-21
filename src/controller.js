2571c2d15c3da46f89d4e1181782ae26608230cb
Theodulf Adegbidi <31116195+tadegbidi@users.noreply.github.com>
2025-09-21 01:14:44 -0400

    Setup project

---
+++ src/controller.js
@@ -0,0 +1,15 @@
+console.log('Connected!');
+
+
+const numContainer = document.querySelectorAll('.numbers div');
+const numbers = new Set(numContainer);
+const operatorsContainer = document.querySelectorAll('.operators div');
+const operators = [...operatorsContainer];
+
+// numbers.forEach(num => num.addEventListener('click', function(e){
+//     console.log(e.target.textContent)
+// }));
+
+class App {
+    
+}
